fc1107f2c646c88a657ee3261d5c8a097a6d7960
Jose Daniel Novoa Garcia <jdnovoa18@gmail.com>
2023-06-21 11:29:41 -0500

    Removed checking if there are or are not collisions

--- Assets/Scripts/SpawnCharacter.cs
+++ Assets/Scripts/SpawnCharacter.cs
@@ -12,77 +12,29 @@ public class SpawnCharacter : MonoBehaviour
     public void SpawnCharacter1()
     {
         Vector3 randomSpawnPosition = GetRandomSpawnPosition();
-        if (CanSpawnAtPosition(randomSpawnPosition))
-        {
-            Instantiate(prefabToSpawn1, randomSpawnPosition, Quaternion.identity);
-        }
+        Instantiate(prefabToSpawn1, randomSpawnPosition, Quaternion.identity);
     }
 
     public void SpawnCharacter2()
     {
         Vector3 randomSpawnPosition = GetRandomSpawnPosition();
-        if (CanSpawnAtPosition(randomSpawnPosition))
-        {
-            Instantiate(prefabToSpawn2, randomSpawnPosition, Quaternion.identity);
-        }
+        Instantiate(prefabToSpawn2, randomSpawnPosition, Quaternion.identity);
     }
+
     public void SpawnCharacter3()
     {
         Vector3 randomSpawnPosition = GetRandomSpawnPosition();
-        if (CanSpawnAtPosition(randomSpawnPosition))
-        {
-            Instantiate(prefabToSpawn3, randomSpawnPosition, Quaternion.identity);
-        }
+        Instantiate(prefabToSpawn3, randomSpawnPosition, Quaternion.identity);
     }
+
     public void SpawnCharacter4()
     {
         Vector3 randomSpawnPosition = GetRandomSpawnPosition();
-        if (CanSpawnAtPosition(randomSpawnPosition))
-        {
-            Instantiate(prefabToSpawn4, randomSpawnPosition, Quaternion.identity);
-        }
+        Instantiate(prefabToSpawn4, randomSpawnPosition, Quaternion.identity);
     }
 
     private Vector3 GetRandomSpawnPosition()
     {
-        return new Vector3(Random.Range(-2f, 3f), 1f, Random.Range(-2f, 3f));
-    }
-
-    private bool CanSpawnAtPosition(Vector3 position)
-    {
-        /*
-        Collider[] colliders = Physics.OverlapSphere(position, 0.5f); // Ajusta el radio según el tamaño del objeto
-
-        foreach (Collider collider in colliders)
-        {
-            if (collider.CompareTag("Respawn"))
-            {
-                return false; // No se puede spawnear aquí porque ya hay un objeto
-            }
-        }
-
-        return true; // Es seguro spawnear en esta posición
-        */
-        Collider[] colliders = Physics.OverlapSphere(position, 0.5f); // Ajusta el radio según el tamaño del objeto
-
-        foreach (Collider collider in colliders)
-        {
-            if (collider.CompareTag("Respawn"))
-            {
-                return false; // No se puede spawnear aquí porque ya hay un objeto
-            }
-        }
-
-        // Buscar una nueva posición si hay colisiones
-        int maxAttempts = 10;
-        int currentAttempt = 0;
-        while (colliders.Length > 0 && currentAttempt < maxAttempts)
-        {
-            position = GetRandomSpawnPosition();
-            colliders = Physics.OverlapSphere(position, 0.5f);
-            currentAttempt++;
-        }
-
-        return colliders.Length == 0; // Retorna true si no hay colisiones
+        return new Vector3(Random.Range(-2f, 0f), 1f, Random.Range(-2f, 3f));
     }
 }
