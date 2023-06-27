3383fe9b8aac2441136fd4be1acf596db22a2f88
Jose Daniel Novoa Garcia <jdnovoa18@gmail.com>
2023-06-27 11:40:18 -0500

    Add to not save changes to the brains variable, and use buttons to simulate a flow in the game

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,10 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] private int brains;
+    public TextMeshProUGUI[] brainsTextArray;
+
+    private void Start()
+    {
+        brains = CountBrains.Instance.Brainss;
+        UpdateBrainsText();
+    }
+    private void UpdateBrainsText()
+    {
+        string brainsString = brains.ToString();
+        foreach (TextMeshProUGUI textMeshProUGUI in brainsTextArray)
+        {
+            textMeshProUGUI.text = brainsString;
+        }
+    }
     public void SelectLevel()
     {
         SceneManager.LoadScene("SelectLevels");
